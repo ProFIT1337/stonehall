7a0f9ff67f36beea1b104536d1aead75eafceea6
ProFIT <santiago.farias@yandex.ru>
2021-03-03 18:09:54 +0700

    refactor about.html

--- blog/urls.py
+++ blog/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 
-from blog.views import BaseView, CategoryDetailView, PostDetailView
+from blog.views import BaseView, CategoryDetailView, PostDetailView, AboutView
 
 
 urlpatterns = [
     path('', BaseView.as_view(), name='base'),
     path('категория/<int:pk>/', CategoryDetailView.as_view(), name='category_detail'),
     path('пост/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('о-нас/', AboutView.as_view(), name='about'),
 ]
 
-# about о-нас
 # projects проекты
 # contacts - контакты
--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,7 @@ from blog.services import get_posts_for_main_page
 
 
 class BaseView(View):
+    """Main page view"""
 
     def get(self, request, *args, **kwargs):
         main_posts = get_posts_for_main_page()
@@ -13,6 +14,13 @@ class BaseView(View):
             'posts': main_posts
         }
         return render(request, 'base.html', context)
+
+
+class AboutView(View):
+    """View with information about firm"""
+
+    def get(self, request, *args, **kwargs):
+        return render(request, 'about.html', {})
 
 
 class CategoryDetailView(DetailView):
@@ -29,4 +37,3 @@ class PostDetailView(DetailView):
     context_object_name = 'post'
     template_name = 'post_detail.html'
     slug_url_kwarg = 'slug'
-
