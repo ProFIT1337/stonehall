f55364d903acbfca3e2755edd5e0d00652d701c6
ProFIT <santiago.farias@yandex.ru>
2021-03-09 03:58:12 +0700

    add favicon

--- blog/admin.py
+++ blog/admin.py
@@ -2,10 +2,4 @@ from django.contrib import admin
 
 from blog.models import Post
 
-
-class PostAdmin(admin.ModelAdmin):
-    save_as = True
-    print(1)
-
-
-admin.site.register(Post, PostAdmin)
+admin.site.register(Post)
--- stonehall/urls.py
+++ stonehall/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
-from . import settings
 from django.contrib.staticfiles.urls import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import path, include
+
+from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
