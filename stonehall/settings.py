0d82bf2f722e1489049961371cca5622384a99d9
ProFIT <santiago.farias@yandex.ru>
2021-03-11 01:24:07 +0700

    fix bugs

--- stonehall/settings.py
+++ stonehall/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('STONEHALL_SECRET_KEY')
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '[::1]']
 
