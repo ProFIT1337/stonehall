ad1315e05a177a8215b941292018075008ab4567
ProFIT <santiago.farias@yandex.ru>
2021-03-23 16:25:24 +0700

    release v1

--- blog/urls.py
+++ blog/urls.py
@@ -3,10 +3,12 @@ from django.views.decorators.cache import cache_page
 
 from blog.views import BaseView, AboutView, PostListView, ContactView, LoginView
 
+from stonehall.settings import CACHE_TIMEOUT
+
 urlpatterns = [
-    path('', cache_page(24 * 60)(BaseView.as_view()), name='base'),
+    path('', cache_page(CACHE_TIMEOUT)(BaseView.as_view()), name='base'),
     path('о-нас/', AboutView.as_view(), name='about'),
-    path('проекты/', cache_page(24 * 60)(PostListView.as_view()), name='projects'),
+    path('проекты/', cache_page(CACHE_TIMEOUT)(PostListView.as_view()), name='projects'),
     path('контакты/', ContactView.as_view(), name='contact'),
     path('логин/', LoginView.as_view(), name='login'),
     path('администрирование/', include('administration.urls'))
